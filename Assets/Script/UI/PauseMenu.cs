73d9dfcd03c8d7b3f281ad388da18a52f3dce466
hoangnmhe153789 <hoangnmhe153789@fpt.edu.vn>
2022-07-20 23:51:09 +0700

    hoangnmhe153789 commit PauseMenu

---
+++ Assets/Script/UI/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPause = false;
+    public GameObject PauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("Loading Menu.....");
+        SceneManager.LoadScene("Menu");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game .....");
+        Application.Quit();
+    }
+
+}
